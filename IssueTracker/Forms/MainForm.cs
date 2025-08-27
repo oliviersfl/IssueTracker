f8fc42591bcacedfda4f3dc4590a24c023052d49
Olivier Sin Fai Lam <olivier.sin.fai.lam@adp.com>
2025-08-27 10:02:48 -0400

    Clear search text after updating/deleting ticket

--- IssueTracker/Forms/MainForm.cs
+++ IssueTracker/Forms/MainForm.cs
@@ -324,6 +324,8 @@ namespace IssueTracker
                     {
                         dgvTickets.ClearSelection();
                     }
+
+                    txtSearch.Text = "";
                 }
             }
         }
